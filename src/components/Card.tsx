56d76fb74e4721a25c3f4eee27b9fec56793a606
codingsagar <sagarishere123@gmail.com>
2024-01-14 19:10:28 +0530

    Fixed some more critical bugs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -37,33 +37,33 @@ export default function NewsCard({ news }: { news: NewsSchema }) {
       }}
     >
       <div className="flex flex-col rounded-lg bg-white dark:bg-slate-900 w-full border border-slate-500 mb-4 p-4 sm:flex-row">
-        {<RenderImage image={news.image_url} />}
+        {<RenderImage image={news?.image_url} />}
         <div className="flex flex-col justify-start p-4">
           <div className="flex flex-wrap gap-2">
             <Chip color="primary" className="rounded-md">
-              {news.creator ? news.creator : "Unknown"}
+              {news?.creator ? news?.creator : "Unknown"}
             </Chip>
             <Chip color="secondary" className="rounded-md">
-              {news.source_id}
+              {news?.source_id}
             </Chip>
             <Chip color="danger" className="rounded-md">
-              {news.category}
+              {news?.category}
             </Chip>
             <Chip color="warning" className="rounded-md">
-              {news.country}
+              {news?.country}
             </Chip>
             <Chip color="success" className="rounded-md">
-              {news.language}
+              {news?.language}
             </Chip>
           </div>
           <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50 mt-4">
-            {news.title}
+            {news?.title}
           </h5>
           <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200 break-words">
-            {news.description.slice(0, 300)}...
+            {news?.description.slice(0, 300)}...
           </p>
           <p className="text-xs text-neutral-500 dark:text-neutral-300">
-            {dayjs(news.pubDate).fromNow()}
+            {dayjs(news?.pubDate).fromNow()}
           </p>
         </div>
       </div>
